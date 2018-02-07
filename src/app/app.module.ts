34dfdcfd15ce9399b0ae4ab889ad6dc8101bad19
Vinicius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-02-06 21:15:19 -0300

    Upando base do projeto com duas componentes (Navbar e Pag home)

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
