f8691a52fdf1f2d73b485465a0f720c6c939669e
Vinícius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-04-08 20:59:21 -0300

    Pequena correção no cadastro de um novo card

---
+++ src/app/models/disciplines.enum.ts
@@ -0,0 +1,6 @@
+export enum Disciplines {
+    LP1 = 'Laboratório de Programação I',
+    LP2 = 'Laboratório de Programação I',
+    LOAC = 'Laboratório de Organização e Arquitetura de Computadores',
+    OAC = 'Organização e Arquitetura de Computadores'
+}
--- src/app/novo-card/novo-card.component.ts
+++ src/app/novo-card/novo-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 
 import { ControllerService } from './../shared/controller.service';
+import { Disciplines } from './../models/disciplines.enum';
 
 @Component({
   selector: 'app-novo-card',
@@ -20,12 +21,20 @@ export class NovoCardComponent implements OnInit {
   public addCard(discipline: string, question: string, answer: string, privacy) {
     const user = this.controller.getUserLogado();
     if (user != null) {
-      this.controller.addNewCard(user.getUsername(), discipline, question, answer, privacy);
-      this.controller.navigate('/perfil');
+      if (this.validaEntrada(question) && this.validaEntrada(answer)) {
+        this.controller.addNewCard(user.getUsername(), discipline, question, answer, privacy);
+        this.controller.navigate('/perfil');
+      } else {
+        alert('Os campos não podem ser vazios!');
+      }
     } else {
       alert('Não existe usuário logado');
       this.controller.navigate('/login');
     }
+  }
+
+  public validaEntrada(value: string): boolean {
+    return value.trim().length !== 0;
   }
 
 }
