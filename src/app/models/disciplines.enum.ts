6c55ab9411981ba3de5f2a6f843eac4afedbcce6
Vinícius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-05-06 14:00:19 -0300

    Update das dependências, e utilização do enum nos cartões

--- src/app/models/disciplines.enum.ts
+++ src/app/models/disciplines.enum.ts
@@ -1,6 +1,7 @@
 export enum Disciplines {
     LP1 = 'Laboratório de Programação I',
-    LP2 = 'Laboratório de Programação I',
+    LP2 = 'Laboratório de Programação II',
     LOAC = 'Laboratório de Organização e Arquitetura de Computadores',
+    PSOFT = 'Projeto de Software',
     OAC = 'Organização e Arquitetura de Computadores'
 }
