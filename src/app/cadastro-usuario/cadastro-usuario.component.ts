2c256d2a1b3fe81c61b617aca524b97d8782aa1a
Vinícius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-05-26 14:59:53 -0300

    Corrigido verificação em relação ao cadastro

--- src/app/cadastro-usuario/cadastro-usuario.component.ts
+++ src/app/cadastro-usuario/cadastro-usuario.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 
 import { ControllerService } from './../shared/controller.service';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-cadastro-usuario',
@@ -9,6 +10,7 @@ import { ControllerService } from './../shared/controller.service';
   styleUrls: ['./cadastro-usuario.component.css']
 })
 export class CadastroUsuarioComponent implements OnInit {
+
 
   constructor(
     private controller: ControllerService
@@ -30,12 +32,39 @@ export class CadastroUsuarioComponent implements OnInit {
         this.controller.addNewUser(username, pswd);
         this.controller.logIn(username);
         this.controller.navigate('/perfil');
-        alert('Usuário criado com sucesso');
+        swal({
+          title: 'Bem-vindo, ' + username,
+          text: 'Aqui, você será capaz de gerênciar sua conta e os seus cartões!',
+          icon: 'success',
+          button: 'Começar'
+        });
       } else {
-        alert('Nome de usuário já cadastrado');
+        swal({
+          title: 'Username inválido',
+          text: 'Já existe usuário cadastrado',
+          icon: 'warning',
+          buttons: [false],
+          timer: 2000
+        });
       }
     } else {
-      alert('Dados inválidos');
+      if (!this.verificaSenha(pswd, pswd2)) {
+        swal({
+          title: 'Dados inválidos',
+          text: 'Senhas diferentes',
+          icon: 'error',
+          buttons: [false],
+          timer: 2000
+        });
+      } else {
+        swal({
+          title: 'Dados inválidos',
+          text: 'Os campos nao podem ser vazios',
+          icon: 'error',
+          buttons: [false],
+          timer: 2000
+        });
+      }
     }
   }
 
