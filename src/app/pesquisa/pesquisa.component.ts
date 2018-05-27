65b20aa5279b65604775a0f4d5567cfe42e7a1c5
Vinícius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-05-27 18:50:59 -0300

    Implementando busca

--- src/app/pesquisa/pesquisa.component.ts
+++ src/app/pesquisa/pesquisa.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 
 import { ControllerService } from './../shared/controller.service';
+import { Subscription } from 'rxjs/Subscription';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pesquisa',
   templateUrl: './pesquisa.component.html',
   styleUrls: ['./pesquisa.component.css']
 })
-export class PesquisaComponent implements OnInit {
+export class PesquisaComponent implements OnInit, OnDestroy {
+
+  private search: string;
+  private subscription: Subscription;
 
   constructor(
-    private controller: ControllerService
+    private controller: ControllerService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+    this.subscription = this.route.queryParams.subscribe(
+      queryParams => this.search = queryParams['search']
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
